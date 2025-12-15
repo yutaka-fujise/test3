2dc2d121f9dda6c447d52330e20c3025a5889ebd
yutaka-fujise <yutaka.f-5112@docomo.ne.jp>
2025-12-15 21:33:03 +0900

    作成

--- src/database/migrations/2025_12_15_204741_create_weight_logs_table.php
+++ src/database/migrations/2025_12_15_204741_create_weight_logs_table.php
@@ -4,28 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWeightLogsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+return new class extends Migration {
     public function up()
     {
         Schema::create('weight_logs', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->date('date');
+            $table->decimal('weight', 5, 1);
+            $table->integer('calorie')->nullable();
+            $table->time('exercise_time')->nullable();
+            $table->text('exercise')->nullable();
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('weight_logs');
     }
-}
+};
